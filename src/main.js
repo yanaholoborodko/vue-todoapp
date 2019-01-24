53202b28d61fc9a0123e2b6bc4bd05b6005d610f
Yana <holoborodkoyana97@gmail.com>
2019-01-24 21:03:43 +0100

    Handling link guards

--- src/main.js
+++ src/main.js
@@ -18,7 +18,29 @@ const router = new VueRouter({
     mode: 'history'
 })
 
-
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+      // this route requires auth, check if logged in
+      // if not, redirect to login page.
+      if (!store.getters.loggedIn) {
+        next({
+          name: 'login',
+        })
+      } else {
+        next()
+      } 
+    } else if (to.matched.some(record => record.meta.requiresVisitor)) {
+        if (store.getters.loggedIn) {
+          next({
+            name: 'todo',
+          })
+        } else {
+          next()
+        } 
+      } else {
+      next() 
+    }
+  })
 
 
 
